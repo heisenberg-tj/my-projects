9ec401de8538f786813e174ad614af67a05a7015
ArcaneKael <dotainvoker12m@gmail.com>
2026-08-11 12:48:11 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -5,4 +5,12 @@ window = tkinter.Tk()
 window.title("Электронные чётки")
 stage_label = tkinter.Label(window, text="Этап: Субханаллах!")
 stage_label.pack()
+count_label = tkinter.Label(window, text=f"Нажатий: {count}")
+count_label.pack()
+def on_click():
+    global count
+    count += 1
+    count_label.config(text=f"Нажатий: {count}")
+button = tkinter.Button(window, text="Нажми", command=on_click)
+button.pack()
 window.mainloop()
