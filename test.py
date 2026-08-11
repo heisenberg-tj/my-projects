86b1eb6712b22f0fa87a75c6fc260f2d78f1383e
ArcaneKael <dotainvoker12m@gmail.com>
2026-08-11 12:34:29 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import tkinter
+count = 0
+stage = 1
+window = tkinter.Tk()
+window.title("Электронные чётки")
+stage_label = tkinter.Label(window, text="Этап: Субханаллах!")
+stage_label.pack()
+window.mainloop()
