67eb48032ba0c9fd4affb6f3cca2486f764b0989
Muhammad <dotainvoker12m@gmail.com>
2026-08-14 01:53:23 +0500

    Add files via upload

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,17 @@
 import random
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&*()_+"
+all_characters = letters + numbers + symbols
+print("----------Генератор супер-паролей----------")
 try:
-    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    numbers = "0123456789"
-    symbols = "!@#$%^&*()_+"
-    all_characters = letters + numbers + symbols
-    print("----------Генератор супер-паролей----------")
     length = int(input("Введите длинну пароля(например, 12): "))
-    password = ""
-    for i in range(length):
-        random_char = random.choice(all_characters)
-        password += random_char
-    print("Ваш пароль:", password)
-    print("ВНИМАНИЕ! Объязательно сохраните этот пароль куда нибудь!")
 except ValueError:
     print("Нужно вводить только цифры!")
+    exit()
+password = ""
+for i in range(length):
+    random_char = random.choice(all_characters)
+    password += random_char
+print("Ваш пароль:", password)
+print("ВНИМАНИЕ! Объязательно сохраните этот пароль куда нибудь!")
